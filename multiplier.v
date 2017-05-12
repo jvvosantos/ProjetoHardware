d2637903eeed538facd363582f22db3591025431
Bleant <biel.bg@gmail.com>
2017-05-12 09:57:42 -0300

    Falta coisa ainda, pera.

--- multiplier.v
+++ multiplier.v
@@ -1,9 +1,13 @@
-module multiplier (/**/)
+module multiplier (clk, fator0, fator1, /**/ HI, LO)
     input clk;
     input reg [31:0] fator0;
     input reg [31:0] fator1;
     
     reg [5:0] count;
+    reg [63:0] resultado;
+    
+    output reg [31:0] HI;
+    output reg [31:0] LO;
     
     
     always @(clk posedge && MultCtrl) begin
@@ -16,5 +20,9 @@ module multiplier (/**/)
             HI <= resultado[63:32];
             LO <= resultado[31:0];
         end
+    end
+    
+    initial begin
+        count <= 0;
     end
 endmodule
