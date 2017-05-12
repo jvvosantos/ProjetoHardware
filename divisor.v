6c4b8e85c36d0e8e0b069bca292c72afcf7481f6
Bleant <biel.bg@gmail.com>
2017-05-12 08:12:01 -0300

    Adicionar comentários, tratar negativos

--- divisor.v
+++ divisor.v
@@ -1,19 +1,38 @@
-input clock;
-input DivCtrl;
-
-input  reg [63:0] dividend;
-input  reg [63:0] divisor;
-output reg [63:0] quotient;
-output reg [63:0] remainder;
-
-
-for(i = 0; i < 33; i= i + 1) begin
-    if(remainder < 0) begin
-        remainder = remainder + divisor;
-        quotient << 1;
-    end else begin
-        quotient << 1;
-    end
+module Divisor (/**/)
+    //Inputs
+    input clk;
+    input DivCtrl;
+    input reg [31:0] divisor;
+    input reg [31:0] dividendo;
     
-    divisor >> 1;
-end
+    reg [63:0] divisorIn;
+    reg [63:0] dividendoIn;
+    reg [31:0] quotient;
+    reg [31:0] quotientCounter;
+    reg [5:0] count;
+    
+    output reg [31:0] HI;
+    output reg [31:0] LO;
+    
+    parameter END <= 6'b000000;
+    
+    always @(clk posedge && DivCtrl) begin
+        if (count < END) begin
+            if (divisorIn <= dividendoIn) begin
+                dividendoIn <= dividendoIn - divisorIn;
+                quotient <= quotient + quotientCounter;
+            end
+            
+            quotientCounter >> 1;
+            divisorIn <= divisorIn >> 1;
+            count <= count + 1;
+        end else begin
+            HI <= dividendoIn [31:0];
+            LO <= quotient;
+    end
+        
+    initial begin
+        divisorIn = {divisor, 32'b00000000000000000000000000000000};
+    end
+            
+endmodule
