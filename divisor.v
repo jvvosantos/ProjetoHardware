4671cc09565c9312fc97ede8ed5036a3da377428
Gabriel Barbosa Gaspar Veloso <gbgv@WINDOWS.CIN.UFPE.BR>
2017-05-13 14:31:15 -0300

    Se alguém não for capaz de ler o nome do arquivo e tiver dúvidas, é o divisor

--- divisor.v
+++ divisor.v
@@ -2,6 +2,7 @@ module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
     //Input
     input clk;
     input DivCtrl;
+	input reset;
     input reg [31:0] divisor;
     input reg [31:0] dividendo;
     
@@ -13,8 +14,12 @@ module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
     reg [31:0] quotient;
     reg [31:0] quotientCounter;
     reg [5:0] count;
-    
+	reg divisorNegativo;
+	reg dividendoNegativo;
+    reg start;
+	
     //Outputs HI e LO
+	output reg divZero;
     output reg [31:0] HI;
     output reg [31:0] LO;
     
@@ -22,26 +27,66 @@ module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
     parameter END <= 6'b100000;
     
     //Executar somente quando DivCtrl estiver ativado
-    always @(clk posedge && DivCtrl) begin
-        if (count < END) begin
-            if (divisorIn <= dividendoIn) begin
-                dividendoIn <= dividendoIn - divisorIn;
-                quotient <= quotient + quotientCounter;
-            end
-            
-            quotientCounter <= quotientCounter >> 1;
-            divisorIn <= divisorIn >> 1;
-            count <= count + 1;
-        end else begin
-            HI <= dividendoIn [31:0];
-            LO <= quotient;
+    always @(clk posedge && (DivCtrl || reset)) begin
+		if (reset) begin
+			divisorIn <= 64'b0;
+			dividendoIn <= 64'b0;
+			quotient <= 32'b0;
+			quotientCounter <= 32'b0;
+			count <= 6'b0;
+			HI <= 32'b0;
+			LO <= 32'b0;
+			start <= 2'b00;
+		end else begin
+			case (start) begin
+				2'b00:
+				begin
+					if (divisor < 0) divisorNegativo <= 1;
+					if (dividendo < 0) dividendoNegativo <= 1;
+					if (divisor == 0) divZero <= 1;
+				end
+				
+				2'b01:
+				begin
+					if (count < END) begin
+						if (divisorIn <= dividendoIn) begin
+							dividendoIn <= dividendoIn - divisorIn;
+							quotient <= quotient + quotientCounter;
+						end
+						
+						quotientCounter <= quotientCounter >> 1;
+						divisorIn <= divisorIn >> 1;
+						count <= count + 1;
+					end else begin
+						HI <= dividendoIn [31:0];
+						LO <= quotient;
+						start <= 2b'10;
+					end
+				end
+				
+				2'b10:
+				begin
+					divisorIn <= 64'b0;
+					dividendoIn <= 64'b0;
+					quotient <= 32'b0;
+					quotientCounter <= 32'b0;
+					count <= 32'b0;
+					HI <= 64'b0;
+					LO <= 64'b0;
+					start <= 2'b0;
+				end
+			endcase
+		end
     end
         
     initial begin
-        //divisorIn a princípio está shifted left 32 vezes
-        divisorIn <= {divisor, 32'b00000000000000000000000000000000};
-        dividendoIn <= {32'b00000000000000000000000000000000, dividendo};
-        quotientCounter <= 32'b10000000000000000000000000000000;
-        count <= 0;
+		divisorIn <= 32'b0;
+		dividendoIn <= 32'b0;
+		quotient <= 32'b0;
+		quotientCounter <= 32'b0;
+		count <= 32'b0;
+		HI <= 32'b0;
+		LO <= 32'b0;
+		start <= 1'b0;
     end
 endmodule
