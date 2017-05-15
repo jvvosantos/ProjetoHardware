a11b2f93e4a2af4257c17481df9a1b4bc55b44ad
Bleant <biel.bg@gmail.com>
2017-05-15 18:13:15 -0300

    Divisor completamente completo!

--- divisor.v
+++ divisor.v
@@ -1,52 +1,52 @@
-module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
+module divisor (clk, reset, DivCtrl, divisor, dividendo, /**/ divZero, HI, LO);
     //Input
     input clk;
     input DivCtrl;
-	input reset;
-    input reg [31:0] divisor;
-    input reg [31:0] dividendo;
+    input reset;
+    input [31:0] divisor;
+    input [31:0] dividendo;
     
-    //divisorIn e dividendoIn sÃ£o registradores para guardar e operar internamente sobre os divisor e dividendo, respectivamente
-    //quotientCounter Ã© um bit para adiÃ§Ã£o ao quociente toda vez que divisorIn for subtraÃ­do de dividendoIn
-    //count conta o nÃºmero de iteraÃ§Ãµes
+    //divisorIn e dividendoIn são registradores para guardar e operar internamente sobre os divisor e dividendo, respectivamente
+    //quotientCounter é um bit para adição ao quociente toda vez que divisorIn for subtraído de dividendoIn
+    //count conta o número de iterações
     reg [63:0] divisorIn;
     reg [63:0] dividendoIn;
     reg [31:0] quotient;
-    reg [31:0] quotientCounter;
+    reg [32:0] quotientCounter;
     reg [5:0] count;
-	reg divisorNegativo;
-	reg dividendoNegativo;
+    reg divisorNegativo;
+    reg dividendoNegativo;
     reg start;
 	
-    //Outputs HI e LO
-	output reg divZero;
+    //Outputs HI e LO 
+    output reg divZero;
     output reg [31:0] HI;
     output reg [31:0] LO;
     
-    //NÃºmero para fim das iteraÃ§Ãµes
-    parameter END <= 6'b100000;
+    //Número para fim das iterações
+    parameter FIN = 6'b100000;
     
     //Executar somente quando DivCtrl estiver ativado
-    always @(clk posedge && (DivCtrl || reset)) begin
+    always @(posedge clk && (DivCtrl || reset)) begin
 		if (reset) begin
 			divisorIn <= 64'b0;
 			dividendoIn <= 64'b0;
 			quotient <= 32'b0;
-			quotientCounter <= 32'b0;
+			quotientCounter <= {1'b1, 32'b0};
 			count <= 6'b0;
 			HI <= 32'b0;
 			LO <= 32'b0;
 			start <= 2'b00;
 		end else begin
-			case (start) begin
+			case (start)
 				2'b00:
 				begin
 					if (divisor[31]) begin
 						divisorNegativo <= 1;
-						divisorIn <= {32'b0, (~divisor + 1)};
+						divisorIn <= {(~divisor + 1), 32'b0};
 					end else begin
 						divisorNegativo <= 0;
-						divisorIn <= {32'b0, divisor};
+						divisorIn <= {divisor, 32'b0};
 					end
 					if (dividendo[31]) begin
 						dividendoNegativo <= 1;
@@ -56,19 +56,23 @@ module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
 						dividendoIn <= {32'b0, dividendo};
 					end
 					
-					if (divisor == 0) divZero <= 1;
-					
 					quotient <= 32'b0;
-					quotientCounter <= 32'b0;
+					quotientCounter <= {1'b1, 32'b0};
 					count <= 6'b0;
 					HI <= 32'b0;
 					LO <= 32'b0;
 					start <= 2'b01;
+					
+					if (divisor == 0) begin
+						divZero <= 1'b1;
+						start <= 2'b00;
+					end
+					else divZero <= 1'b0;
 				end
 				
 				2'b01:
 				begin
-					if (count < END) begin
+					if (count <= FIN) begin
 						if (divisorIn <= dividendoIn) begin
 							dividendoIn <= dividendoIn - divisorIn;
 							quotient <= quotient + quotientCounter;
@@ -85,9 +89,9 @@ module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
 						end else begin
 							if (dividendoNegativo) HI <= (~dividendoIn [31:0]) + 1;
 							else HI <= dividendoIn [31:0];
-							LO < (~quotient + 1);
+							LO <= (~quotient + 1);
 						end
-						start <= 2b'10;
+						start <= 2'b10;
 					end
 				end
 				
@@ -96,7 +100,7 @@ module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
 					divisorIn <= 64'b0;
 					dividendoIn <= 64'b0;
 					quotient <= 32'b0;
-					quotientCounter <= 32'b0;
+					quotientCounter <= {1'b1, 32'b0};
 					count <= 32'b0;
 					start <= 2'b0;
 				end
@@ -108,7 +112,7 @@ module divisor (clk, DivCtrl, divisor, dividendo, /**/ HI, LO)
 		divisorIn <= 32'b0;
 		dividendoIn <= 32'b0;
 		quotient <= 32'b0;
-		quotientCounter <= 32'b0;
+		quotientCounter <= {1'b1, 32'b0};
 		count <= 32'b0;
 		HI <= 32'b0;
 		LO <= 32'b0;
