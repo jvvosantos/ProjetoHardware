01c268217eede149333edb209c11c880684a79fa
Bleant <biel.bg@gmail.com>
2017-05-15 22:45:55 -0300

    Corrigidos erros de sintaxe.

--- ShiftLeft2.v
+++ ShiftLeft2.v
@@ -1,8 +1,8 @@
-module ShiftLeft2 (inPut, /**/ outPut) begin
+module ShiftLeft2 (inPut, /**/ outPut); 
 	input [25:0] inPut;
 	output [27:0] outPut;
 
 	always @(*) begin
 		outPut = {inPut, 2'b0};
 	end
-end
+endmodule
