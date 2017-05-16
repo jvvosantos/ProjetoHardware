96e7af0ad338c480258428e558833f1183f22264
Marina Oliveira de Barros <mob@WINDOWS.CIN.UFPE.BR>
2017-05-16 14:29:10 -0300

    Correcao do ShiftNMux

--- ShiftNMux.v
+++ ShiftNMux.v
@@ -1,8 +1,8 @@
 module ShiftNMux (ShiftN, in_shamt, in_funct, mux_out);
 
 input [1:0] ShiftN;
-input [31:0] in_shamt;
-input [31:0] in_funct;
+input [4:0] in_shamt;
+input [4:0] in_funct;
 output reg [31:0] mux_out;
 
 always @(ShiftN) begin
@@ -10,11 +10,11 @@ always @(ShiftN) begin
 	case (ShiftN)
 		2'b00: 
 			begin
-				mux_out <= in_regA;
+				mux_out <= in_shamt;
 			end
 		2'b01: 
 			begin
-				mux_out <= in_regB;
+				mux_out <= in_funct;
 			end
 		2'b10: 
 			begin
